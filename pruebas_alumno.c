9b3db20b3751b47686dcff72484c5ec4a280a866
ifabregat <ifabregat@fi.uba.ar>
2024-11-24 18:51:59 -0300

    End with test for menu and pokedex

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -38,9 +38,10 @@ void agregarOpciones()
 	char *descripcion = "Opcion a";
 	char *descripcion2 = "Opcion b";
 
-	pa2m_afirmar(menu_agregar_opcion(menu, 'a', descripcion, NULL), "Puedo agregar una opcion al menu");
-	pa2m_afirmar(menu_agregar_opcion(menu, 'b', descripcion2, NULL), "Puedo agregar una segunda opcion al menu");
-
+	pa2m_afirmar(menu_agregar_opcion(menu, 'a', descripcion, NULL),
+		     "Puedo agregar una opcion al menu");
+	pa2m_afirmar(menu_agregar_opcion(menu, 'b', descripcion2, NULL),
+		     "Puedo agregar una segunda opcion al menu");
 
 	menu_destruir(menu, destructor);
 }
@@ -55,8 +56,10 @@ void elegirOpcion()
 	menu_agregar_opcion(menu, 'a', descripcion, imprimirA);
 	menu_agregar_opcion(menu, 'b', descripcion2, imprimirB);
 
-	pa2m_afirmar(menu_ejecutar_opcion(menu, 'a'), "Puedo elegir la opcion a");
-	pa2m_afirmar(menu_ejecutar_opcion(menu, 'b'), "Puedo elegir la opcion b");
+	pa2m_afirmar(menu_ejecutar_opcion(menu, 'a'),
+		     "Puedo elegir la opcion a");
+	pa2m_afirmar(menu_ejecutar_opcion(menu, 'b'),
+		     "Puedo elegir la opcion b");
 
 	menu_destruir(menu, destructor);
 }
@@ -92,8 +95,7 @@ void seleccionarOpcion()
 
 	char opcion = 0;
 
-	if (scanf("%c", &opcion) != 1)
-	{
+	if (scanf("%c", &opcion) != 1) {
 		printf("Error al leer la opcion\n");
 	}
 
@@ -124,13 +126,15 @@ void agregarPokemones()
 {
 	pokedex_t *pokedex = pokedex_crear();
 
-	pa2m_afirmar(pokedex_agregar_pokemon(pokedex, "datos/pokedex.csv"), "Puedo agregar pokemones al pokedex");
+	pa2m_afirmar(pokedex_agregar_pokemon(pokedex, "datos/pokedex.csv"),
+		     "Puedo agregar pokemones al pokedex");
 
 	size_t cantidad = pokedex_cantidad(pokedex);
 
 	printf("Cantidad de pokemones: %ld\n", cantidad);
 
-	pa2m_afirmar(pokedex_cantidad(pokedex) == 10, "La cantidad de pokemones es la correcta");
+	pa2m_afirmar(pokedex_cantidad(pokedex) == 10,
+		     "La cantidad de pokemones es la correcta");
 
 	pokedex_destruir_todo(pokedex, destructor_pokemones);
 }
@@ -160,8 +164,7 @@ void obtenerSietePokemonesAleatorios()
 
 	pokedex_agregar_pokemon(pokedex, "datos/pokedex.csv");
 
-	for (int i = 0; i < 7; i++)
-	{
+	for (int i = 0; i < 7; i++) {
 		pokemon_t *pokemon = pokedex_obtener_pokemon_random(pokedex);
 
 		printf("Pokemon aleatorio: %s\n", pokemon->nombre);
@@ -172,7 +175,8 @@ void obtenerSietePokemonesAleatorios()
 
 bool imprimir_pokemon(pokemon_t *pokemon, void *ctx)
 {
-	printf("%s%s%s%s", ANSI_COLOR_BOLD, pokemon->color, pokemon->nombre, ANSI_COLOR_RESET);
+	printf("%s%s%s%s", ANSI_COLOR_BOLD, pokemon->color, pokemon->nombre,
+	       ANSI_COLOR_RESET);
 
 	printf("%s (%s)", ANSI_COLOR_BOLD, pokemon->movimientos);
 
@@ -187,6 +191,8 @@ void mostrarPokemones()
 
 	pokedex_agregar_pokemon(pokedex, "datos/pokedex.csv");
 
+	printf("\n");
+
 	pokedex_iterar_pokemones(pokedex, imprimir_pokemon, NULL);
 
 	pokedex_destruir_todo(pokedex, destructor_pokemones);
@@ -194,27 +200,23 @@ void mostrarPokemones()
 
 void menuMostrarPokedex()
 {
-	pokedex_t *pokedex = pokedex_crear();
-
-	pokedex_agregar_pokemon(pokedex, "datos/pokedex.csv");
-
 	menu_t *menu = menu_crear();
 
-	menu_agregar_opcion(menu, 'P', "Mostrar pokedex", (void (*)(void))mostrarPokemones);
-	
+	char *descripcion = "Pokedex";
+
+	menu_agregar_opcion(menu, 'p', descripcion, mostrarPokemones);
+
 	menu_mostrar(menu);
 
 	char opcion = 0;
 
-	if (scanf("%c", &opcion) != 1)
-	{
+	if (scanf(" %c", &opcion) != 1) {
 		printf("Error al leer la opcion\n");
 	}
 
 	menu_ejecutar_opcion(menu, opcion);
 
 	menu_destruir(menu, destructor);
-	pokedex_destruir_todo(pokedex, destructor_pokemones);
 }
 
 int main()
@@ -239,6 +241,7 @@ int main()
 	obtenerPokemonAleatorio();
 	printf("\n");
 	obtenerSietePokemonesAleatorios();
+	printf("\n");
 	menuMostrarPokedex();
 
 	return pa2m_mostrar_reporte();
